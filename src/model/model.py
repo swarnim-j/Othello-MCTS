e43c105d6e1426f2785edba808bdb9bebad5ee59
Swarnim Jain <swarnimjain004@gmail.com>
2023-12-31 10:29:46 +0530

    restructured project + added model.py

--- src/model/model.py
+++ src/model/model.py
@@ -6,8 +6,7 @@ class OthelloNet(nn.Module):
     def __init__(self, game, args):
         super(OthelloNet, self).__init__()
 
-        self.board_x, self.board_y = game.get_board_size()
-        self.action_size = game.get_action_size()
+        self.x, self.y = game.getBoardSize()
         self.args = args
         channels = args.num_channels
 
@@ -17,41 +16,42 @@ class OthelloNet(nn.Module):
         self.conv3 = nn.Conv2d(channels, channels, 3, stride=1)
         self.conv4 = nn.Conv2d(channels, channels, 3, stride=1)
 
-        # Batch normalization
+        # Batch normalization layers
         self.bn1 = nn.BatchNorm2d(channels)
         self.bn2 = nn.BatchNorm2d(channels)
         self.bn3 = nn.BatchNorm2d(channels)
         self.bn4 = nn.BatchNorm2d(channels)
 
         # Fully connected layers
-        self.fc1 = nn.Linear(channels * (self.board_x - 4) * (self.board_y - 4), 1024)
+        self.fc1 = nn.Linear(channels * (self.x - 4) * (self.y - 4), 1024)
         self.fc_bn1 = nn.BatchNorm1d(1024)
         self.fc2 = nn.Linear(1024, 512)
         self.fc_bn2 = nn.BatchNorm1d(512)
 
         # Output layers
-        self.fc3 = nn.Linear(512, self.action_size)  # Policy head
-        self.fc4 = nn.Linear(512, 1)  # Value head
+        self.fc3 = nn.Linear(512, game.getActionSize()) # Policy head
+        self.fc4 = nn.Linear(512, 1)                    # Value head
 
     def forward(self, s):
         # Input reshaping
-        s = s.view(-1, 1, self.board_x, self.board_y)
+        s = s.view(-1, 1, self.x, self.y)
 
-        # Convolutional layers with batch normalization and ReLU activation
+        # Convolutional layers + batch normalization + ReLU
         s = F.relu(self.bn1(self.conv1(s)))
         s = F.relu(self.bn2(self.conv2(s)))
         s = F.relu(self.bn3(self.conv3(s)))
         s = F.relu(self.bn4(self.conv4(s)))
 
-        # Flatten before fully connected layers
-        s = s.view(-1, self.args.num_channels * (self.board_x - 4) * (self.board_y - 4))
+        # Flatten
+        s = s.view(-1, self.args.num_channels * (self.x - 4) * (self.y - 4))
 
-        # Fully connected layers with dropout
+        # Fully connected layers + batch normalization + ReLU + dropout
         s = F.dropout(F.relu(self.fc_bn1(self.fc1(s))), p=self.args.dropout, training=self.training)
         s = F.dropout(F.relu(self.fc_bn2(self.fc2(s))), p=self.args.dropout, training=self.training)
 
-        # Policy and value heads
+        # Output layers
         pi = self.fc3(s)
         v = self.fc4(s)
 
+        # Return policy and value
         return F.log_softmax(pi, dim=1), torch.tanh(v)
