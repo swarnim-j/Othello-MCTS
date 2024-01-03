7c135b5158b83d2b53f19c0ad77ac18588da9aa3
Swarnim Jain <swarnimjain004@gmail.com>
2024-01-03 11:58:28 +0530

    added play.py

---
+++ play.py
@@ -0,0 +1,47 @@
+from src.game.game import Game
+from src.model.model import OthelloModel
+from src.MCTS import MCTS
+from src.train.arena import Arena
+from src.train.players import *
+
+class Args:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+args_dict = {
+    'size': 6, # size of the board
+    'opponent': 'mcts', # opponent to play against
+    # TODO: add arguments
+}
+
+args = Args(**args_dict)
+
+def main():
+    player1 = HumanPlayer()
+    player2 = None
+
+    game = Game(args.size)
+
+    if args.opponent == 'mcts':
+        model = OthelloModel(game)
+        # TODO: change file and folder names
+        model.loadCheckpoint('./temp/', 'best.pth.tar')
+        mcts = MCTS(game, model, args)
+        player2 = MCTSPlayer(mcts)
+
+    elif args.opponent == 'random':
+        player2 = RandomPlayer()
+
+    elif args.opponent == 'greedy':
+        player2 = GreedyPlayer()
+
+    else:
+        raise ValueError('Invalid opponent')
+    
+    arena = Arena(player1, player2, game)
+
+    arena.playGames(args.num_games)
+
+if __name__ == "__main__":
+    main()
--- src/train/arena.py
+++ src/train/arena.py
@@ -16,11 +16,15 @@ class Arena:
             self.player2 = player2
             self.game = game
 
-    def playGame(self) -> int:
+    def playGame(self, print_board: bool = False) -> int:
             """
             Executes one episode of a game.
+
+            Args:
+                print_board (bool): Flag to indicate whether to print the board during the game.
+
             Returns:
-                Integer with the result of the game for player1.
+                int: Integer with the result of the game for player1.
             """
             board = self.game.getInitBoard()
 
@@ -41,46 +45,56 @@ class Arena:
                 if valids[move] != 0:
                     board, current_player = self.game.nextState(board, move)
 
+                # Print board
+                if print_board:
+                    print("Move ", i, "Player ", current_player)
+                    board.printBoard()
+
             # Get result
             result = self.game.hasGameEnded(self.board, current_player)
 
+            # Print result
+            if print_board:
+                print("Game over: Turn ", i, "Result ", result)
+
             return result * current_player
     
-    def playGames(self, num_games: int) -> (int, int, int):
-            """
-            Play a specified number of games between two players and return the results.
+    def playGames(self, num_games: int, print_board: bool = False) -> (int, int, int):
+        """
+        Play a specified number of games between two players and return the results.
 
-            Args:
-                num_games (int): The number of games to be played.
+        Args:
+            num_games (int): The number of games to be played.
+            print_board (bool): Flag indicating whether to print the board after each move. Default is False.
 
-            Returns:
-                tuple: A tuple containing the number of wins for player 1, player 2, and draws, respectively.
-            """
-            num = num_games // 2
-            wins = [0, 0]
-            draws = 0
+        Returns:
+            tuple: A tuple containing the number of wins for player 1, player 2, and draws, respectively.
+        """
+        num = num_games // 2
+        wins = [0, 0]
+        draws = 0
 
-            for _ in tqdm(range(num), desc="Arena.playGames (P1 starts)"):
-                result = self.playGame()
-                wins[0] += result
-                if result == 0:
-                    draws += 1
-                elif result == -1:
-                    wins[1] += 1
-                else:
-                    wins[0] += 1
+        for _ in tqdm(range(num), desc="Arena.playGames (P1 starts)"):
+            result = self.playGame(print_board)
+            wins[0] += result
+            if result == 0:
+                draws += 1
+            elif result == -1:
+                wins[1] += 1
+            else:
+                wins[0] += 1
 
-            self.player1, self.player2 = self.player2, self.player1
+        self.player1, self.player2 = self.player2, self.player1
 
-            for _ in tqdm(range(num), desc="Arena.playGames (P2 starts)"):
-                result = self.playGame()
-                wins[0] += result
-                if result == 0:
-                    draws += 1
-                elif result == -1:
-                    wins[0] += 1
-                else:
-                    wins[1] += 1
+        for _ in tqdm(range(num), desc="Arena.playGames (P2 starts)"):
+            result = self.playGame(print_board)
+            wins[0] += result
+            if result == 0:
+                draws += 1
+            elif result == -1:
+                wins[0] += 1
+            else:
+                wins[1] += 1
 
-            return (*wins, draws)
+        return (*wins, draws)
 
